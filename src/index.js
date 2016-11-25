f8aed325895ef604c04bc3cd2f0fe6c6fcd78ea4
laqmarc <laqmarc@gmail.com>
2016-11-25 18:07:32 +0100

    fix end lines

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 export default (target) => {
-  
-  target.style.margin = (((target.parentElement.offsetHeight - target.offsetHeight) / 2) | 0) + 'px auto'
-  target.style.position = 'absolute'
+
+    target.style.margin = (((target.parentElement.offsetHeight - target.offsetHeight) / 2) | 0) + 'px auto'
+    target.style.position = 'absolute'
 }
-
-
-
