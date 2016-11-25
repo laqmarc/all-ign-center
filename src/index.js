fe6fb00ee9b27d1e674a3b4dc9d1acfa5d065bef
laqmarc <laqmarc@gmail.com>
2016-11-25 15:44:57 +0100

    quick fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const allignCenter = target => {
   
   target.style.margin = (((target.parentElement.offsetHeight - target.offsetHeight) / 2) | 0) + 'px auto'
+  target.style.position = 'absolute'
 }
 
 export default allignCenter
