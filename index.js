503dd6862b42366d8b8c84416560a2678fc5215a
laqmarc <laqmarc@gmail.com>
2016-11-25 15:49:44 +0100

    build

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const allignCenter = target => {
   
   target.style.margin = (((target.parentElement.offsetHeight - target.offsetHeight) / 2) | 0) + 'px auto';
+  target.style.position = 'absolute';
 };
 
 module.exports = allignCenter;
